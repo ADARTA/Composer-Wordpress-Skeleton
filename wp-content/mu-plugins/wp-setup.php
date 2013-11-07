2e5e1b11a628b9c94edba47b4b1f090d769e00e3
talves <tony@3alves.com>
2013-11-07 08:35:06 -0800

    Add must use plugin for composer setup options

--- wp-content/mu-plugins/wp-setup.php
+++ wp-content/mu-plugins/wp-setup.php
@@ -4,12 +4,18 @@ if (!is_blog_installed()) { return; }
 $my_server_port = ((!empty ($_SERVER ['SERVER_PORT']) AND $_SERVER ['SERVER_PORT'] <> '80') ? (":" . $_SERVER ['SERVER_PORT']) : '');
 $my_server_name = (!empty ($my_server_port) ?($_SERVER['SERVER_NAME'] . $my_server_port) : $_SERVER['SERVER_NAME'] );
 
-if ('http://' . $my_server_name . '/wp' == get_option('home') || (empty (get_option('upload_url_path')))) {
+if ('http://' . $my_server_name . '/wp' == get_option('home')) {
   update_option('siteurl', 'http://' . $my_server_name . '/wp');
   update_option('home', 'http://' . $my_server_name);
   update_option('upload_path', $_SERVER['DOCUMENT_ROOT'] . '\media');
   update_option('upload_url_path', 'http://' . $my_server_name . '/media');
   update_option('permalink_structure', '/%postname%/');
+}
+if (get_option('upload_url_path') == ''){
+  update_option('upload_url_path', 'http://' . $my_server_name . '/media');  
+}
+if (get_option('upload_path') == '' || get_option('upload_path') == 'media'){
+  update_option('upload_path', $_SERVER['DOCUMENT_ROOT'] . '\media');  
 }
  
 if (defined('FRONT_PAGE') && defined('POSTS_PAGE') && !get_option('page_on_front')) {
